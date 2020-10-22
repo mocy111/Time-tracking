763786413189fc2a03c52c311ddb443149a9aee8
MOCTAR YONLI <yonli.moctar@codeloccol.org>
2020-10-22 18:19:59 +0100

    Updates

---
+++ precache-manifest.2ae609c18b47553dc247ce8a3c1ee71c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4badac321a109e4ec13664bde5cb9d2",
+    "url": "/time-tracking/index.html"
+  },
+  {
+    "revision": "e1d8bf8374375274e86e",
+    "url": "/time-tracking/static/css/main.9e025c97.chunk.css"
+  },
+  {
+    "revision": "46173509b2791d7935fd",
+    "url": "/time-tracking/static/js/2.6d6d2137.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/time-tracking/static/js/2.6d6d2137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d8bf8374375274e86e",
+    "url": "/time-tracking/static/js/main.2ebc5d40.chunk.js"
+  },
+  {
+    "revision": "eb1ee739158b79455e9d",
+    "url": "/time-tracking/static/js/runtime-main.3e0c5302.js"
+  }
+]);
